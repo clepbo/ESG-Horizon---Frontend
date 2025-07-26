026482df7b917a405a53aaa094ec81082f9516c1
Aghahowa Favour AIGBEKAEN <aigbekaenfavour@gmail.com>
2025-07-26 09:04:07 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import LoginPage from "@/app/login/page";
-=======
-import TestComponent from "./components/TestComponent";
->>>>>>> 90168276d9690f74d885924df49abbdda6bbdf42
 
 export default function Home() {
   return (
     <>
-<<<<<<< HEAD
       <LoginPage />
-=======
-      <TestComponent />
->>>>>>> 90168276d9690f74d885924df49abbdda6bbdf42
     </>
   );
 }
