9a3357d9129ef16ba53b022faca6f79423e99d85
sadiqasg <sadiqasg@gmail.com>
2025-10-09 08:46:48 +0100

    v1.02: fix linting

--- app/(company)/dashboard-esg/page.tsx
+++ app/(company)/dashboard-esg/page.tsx
@@ -12,7 +12,6 @@ import ReportTable from "../components/ReportTab";
 import ESGTour from "@/app/components/company/ESGTour";
 import { useAuth } from "@/context/AuthContext";
 import { motion } from "framer-motion";
-
 import { useCompanyDashboard } from "@/services/hooks/dashboard.hooks";
 
 export default function DashboardPage() {
@@ -169,6 +168,7 @@ export default function DashboardPage() {
                     </div>
                 </div>
 
+                {/* Activity Table */}
                 <div className="space-y-4">
                     <h2 className="text-xl font-semibold text-gray-900">
                         Recent Reports
