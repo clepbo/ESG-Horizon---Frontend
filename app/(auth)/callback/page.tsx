a485c65bd487a691326d4d4a2766f5db798b2a76
sadiqasg <sadiqasg@gmail.com>
2025-10-14 14:28:41 +0100

    prettify

--- app/(auth)/callback/page.tsx
+++ app/(auth)/callback/page.tsx
@@ -41,6 +41,7 @@ export default function CallbackPage() {
 
         toast.success("Logged in successfully!");
         router.push("/dashboard-esg");
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
       } catch (err: any) {
         const message =
           err?.response?.data?.message || err?.message || "Login failed. Please try again.";
--- app/components/company/subsidiaries/SubsidiaryTable.tsx
+++ app/components/company/subsidiaries/SubsidiaryTable.tsx
@@ -101,16 +101,17 @@ export default function SubsidiaryTable({ subsidiaries, onDelete, onEdit }: Subs
                 ))}
               </tbody>
             </table>
-            {/* pagination */}
-            <div className="mt-4 px-4 pb-4">
-              <Pagination
-                totalItems={subsidiaries.length}
-                itemsPerPage={itemsPerPage}
-                currentPage={currentPage}
-                onPageChange={setCurrentPage}
-                onItemsPerPageChange={setItemsPerPage}
-              />
-            </div>
+          </div>
+
+          {/* pagination */}
+          <div className="mt-4 px-4 pb-4">
+            <Pagination
+              totalItems={subsidiaries.length}
+              itemsPerPage={itemsPerPage}
+              currentPage={currentPage}
+              onPageChange={setCurrentPage}
+              onItemsPerPageChange={setItemsPerPage}
+            />
           </div>
         </section>
       )}
