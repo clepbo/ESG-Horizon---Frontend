03a6b1764ce060160425499685ef137de1da0bee
sadiqasg <sadiqasg@gmail.com>
2025-10-23 09:17:04 +0100

    remove double no logo message

--- app/(company)/components/Sidebar.tsx
+++ app/(company)/components/Sidebar.tsx
@@ -98,10 +98,6 @@ export default function Sidebar() {
           <span className="text-sm font-medium text-white">
             {user?.company?.name || "Company Name"}
           </span>
-
-          {!user?.company?.company_logo_url && user?.company?.name && (
-            <span className="text-xs text-white/50">— Logo Missing</span>
-          )}
         </div>
 
         {/* <div className="mb-1 hidden md:flex items-center space-x-2 bg-teal-600 rounded-md px-3 py-2">
