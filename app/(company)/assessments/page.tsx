a35082cfbc5d6a8db36402b3fd785e10b5a8d3d9
sadiqasg <sadiqasg@gmail.com>
2025-11-05 09:19:35 +0100

    fix(assessment): contents

--- app/(company)/assessments/page.tsx
+++ app/(company)/assessments/page.tsx
@@ -62,20 +62,37 @@ export default function NewAssessmentPage() {
             Track your ESG data collection progress across all pillars
           </p>
         </div>
-        <Card className="max-w-6xl w-full p-12 text-center rounded-md bg-white border-none mb-10 shadow-md">
-          <CardContent>
-            <h1 className="text-2xl font-bold mb-6 text-neutral-1000">Start a New Assessment</h1>
-            <p className="text-sm text-gray-600 mb-10 leading-relaxed">
-              Start a fresh assessment to measure your company&apos;s environmental, social, and
-              governance performance. Select the scope, reporting period, and assign
-              responsibilities to your team.
-            </p>
+        <Card className="max-w-4xl w-full mx-auto p-15 rounded-md bg-white border-none mb-10 shadow-md">
+          <CardContent className="flex flex-col items-center justify-center">
+            <h1 className="text-2xl font-bold mb-6 text-neutral-1000">Welcome To Assessment</h1>
+            {assessments && assessments.length > 0 ? (
+              <p className="text-sm text-gray-600 mb-6 leading-relaxed text-center">
+                Click on the button below to continue with creating assessments.
+              </p>
+            ) : (
+              <p className="text-sm text-gray-600 mb-6 leading-relaxed text-center">
+                It looks like you haven&apos;t started any yet. Click on the
+                <br />
+                button below to get started.
+              </p>
+            )}
+
             <Button
               onClick={() => router.push("/assessments/hub")}
-              className="bg-[var(--color-primary)] transform hover:scale-[1.02] text-white  px-8 py-4 text-sm rounded-sm"
+              className="bg-[var(--color-primary)] transform hover:scale-[1.02] text-white px-8 py-4 text-sm rounded-sm"
             >
               Start New Assessment
             </Button>
+            <p className="mt-6 text-sm text-gray-600 mb-10 leading-relaxed text-center">
+              Or, have a lot of data? You can also{" "}
+              <strong
+                onClick={() => alert("Coming soon!")}
+                style={{ cursor: "pointer", color: "black", textDecoration: "none" }}
+              >
+                Bulk Upload
+              </strong>{" "}
+              our assessments.
+            </p>
           </CardContent>
         </Card>
 
