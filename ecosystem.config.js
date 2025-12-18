1c7e7aaee354d7796e24c86a58bea23f4bfbb723
sadiqasg <sadiqasg@gmail.com>
2025-12-18 21:28:36 +0100

    final fixes: ci and ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,10 @@ module.exports = {
     {
       name: "esg-frontend-prod",
       script: "./server.js",
-      instances: "max",
-      exec_mode: "cluster",
+      // Change 'max' to 1 for a 1vCPU server to save resources
+      instances: 1, 
+      // 'fork' mode is usually more stable than 'cluster' for single-instance setups
+      exec_mode: "fork", 
       env: {
         NODE_ENV: "production",
         PORT: 3001,
